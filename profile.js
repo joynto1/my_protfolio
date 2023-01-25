e6509c5c8e2f671310c3ed12f303adef991ad010
joynto1 <joynto92@gmail.com>
2023-01-25 13:47:33 +0600

    y second commit

---
+++ profile.js
@@ -0,0 +1,11 @@
+////////////sidebar/////////////////
+function sidebar(){
+  var navbar = document.getElementById("navbar");
+ setTimeout(function(){
+  navbar.classList.toggle("show");
+ },
+90
+ );
+};
+
+S
